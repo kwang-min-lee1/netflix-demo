6f469e0ed3adf5c183ab138b8b19432fe71c07a7
Lee Kwang Min <minwinn1@gmail.com>
2026-04-12 09:38:19 +0900

    배너 디자인

--- src/pages/Homepage/components/Banner/Banner.jsx
+++ src/pages/Homepage/components/Banner/Banner.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 import { usePopularMoviesQuery } from "../../../../hooks/usePopularMovies";
+import Alert from "react-bootstrap/Alert";
+import "./Banner.style.css"
 
 const Banner = () => {
-  const { data } = usePopularMoviesQuery();
+  const { data, isLoading, isError, error } = usePopularMoviesQuery();
   console.log("ddd", data);
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+  if (isError) {
+    return <Alert variant="danger">{error.message}</Alert>;
+  }
 
-  return <div>Banner</div>;
+  return (
+    <div
+      style={{
+        backgroundImage:
+          "url(" +
+          `https://media.themoviedb.org/t/p/w533_and_h300_face${data?.results[0].poster_path}` +
+          ")",
+      }}
+      className="banner"
+    >
+      <div className="text-white banner-text-area">
+        <h1>{data?.results[0].title}</h1>
+        <p>{data?.results[0].overview}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
