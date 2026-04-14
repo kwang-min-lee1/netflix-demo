fb3d94522399eb4a1a4731b83fb564f4e8b5581c
Lee Kwang Min <minwinn1@gmail.com>
2026-04-15 01:41:20 +0900

    키워드로 영화 찾기

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet, Link, useNavigate } from 'react-router-dom';
 
 const AppLayout = () => {
+
+  const [keyword, setKeyword] = useState("")
+  const navigate = useNavigate()
+
+  const searchByKeyword=(event)=>{
+    event.preventDefault()   // form 이므로 항상 작성 -> 리프레시를 막아줌
+    // url을 바꿔주기
+    navigate(`/movies?q=${keyword}`)
+    setKeyword("")   // 서치박스에 setKeyword 값 비워주기
+  }
+ 
   return (
     <div style={{ backgroundColor: "black", minHeight: "100vh", color: "white" }}>
      <Navbar  variant="dark" expand="lg" style={{ backgroundColor: "#000" }} >
@@ -31,14 +42,18 @@ const AppLayout = () => {
            
             
           </Nav>
-          <Form className="d-flex">
+          <Form className="d-flex" onSubmit={searchByKeyword}>
             <Form.Control
               type="search"
               placeholder="Search"
               className="me-2"
               aria-label="Search"
+              value={keyword}
+              onChange={(event) => setKeyword(event.target.value)}
             />
-            <Button variant="outline-danger">Search</Button>
+            <Button variant="outline-danger" type="submit">
+              Search
+            </Button>
           </Form>
         </Navbar.Collapse>
       </Container>
--- src/pages/Movies/MoviePage.jsx
+++ src/pages/Movies/MoviePage.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from "react";
+import { useSearchMovieQuery } from "../../hooks/useSearchMovie";
+import { useSearchParams } from "react-router-dom";
+import { Col, Container, Row,  Spinner } from "react-bootstrap";
+import MovieCard from "../../common/MovieCard/MovieCard";
+Col
+
+// 경로 2가지
+// nav바에서 클릭해서 온 경우 => popularMovie 보여주기
+// keyword를 입력해서 온 경우 => keyword와 관련된 영화들을 보여줌
+
+// 페이지네이션 설치
+// page state 만들기
+// 페이지네이션 클릭할때 마다 page 바꿔주기
+// page값이 바뀔때 마다 useSearchMovie에 page까지 넣어서 fetch
 
 const MoviePage = () => {
-  return (
-    <div>MoviePage</div>
-  )
-}
+  const [query, setQuery] = useSearchParams();
+  const keyword = query.get("q");
 
-export default MoviePage
+  const { data, isLoding, isError, error } = useSearchMovieQuery({ keyword });
+  console.log("ddd", data);
+
+  if (isLoding) {
+    return (
+      <div className="spinner-area">
+        <Spinner
+          animation="border"
+          variant="danger"
+          style={{ width: "5rem", height: "5rem" }}
+        />
+      </div>
+    );
+  }
+
+  if (isError) {
+    return <Alert variant="danger">{error.message}</Alert>;
+  }
+
+  return (
+    <Container>
+      <Row>
+        
+        <Col lg={4} xs={12}>
+          {" "}
+          필터{" "}
+        </Col>
+        
+        <Col lg={8} xs={12}>
+          <Row>
+            {data?.results.map((movie, index) => (
+              <Col key={index} lg={4} xs={12}>
+                <MovieCard movie={movie} />
+              </Col>
+            ))}
+          </Row>
+        </Col>
+
+      </Row>
+    </Container>
+  );
+};
+
+export default MoviePage;
